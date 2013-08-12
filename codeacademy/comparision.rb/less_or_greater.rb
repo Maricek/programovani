5193ec58e4c8fe1688136f21ae80cecc7a4b80d5
Mariana <trantinovamariana@gmail.com>
2013-08-12 03:53:14 +0200

    Zlepseni v poznamkach

--- codeacademy/comparision.rb/less_or_greater.rb
+++ codeacademy/comparision.rb/less_or_greater.rb
@@ -1,7 +1,20 @@
+=begin
+  Less Than or Greater Than
+  We can also check to see if one value is less than, less than or equal to, greater than, or greater than or equal to another. Those operators look like this:
+                                                                                                                                                              Less than: <
+  Less than or equal to: <=
+  Greater than: >
+  Greater than or equal to: >=
+=end
+
+# Use bigger than operator
 test_1 = 17 > 16
 
+# Use less than operator
 test_2 = 21 < 30
 
-test_3 = 9 == 9
+# Use greater or equal operator
+test_3 = 9 >= 9
 
+# Use less than or equal operator
 test_4 = -11 <= 4
