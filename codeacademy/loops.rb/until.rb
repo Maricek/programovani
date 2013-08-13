ff220153115d5123bfd5540377df8e044e93616a
Mariana <trantinovamariana@gmail.com>
2013-08-14 00:04:11 +0200

    Pridany poznamky k cyklu until

--- codeacademy/loops.rb/until.rb
+++ codeacademy/loops.rb/until.rb
@@ -1,6 +1,12 @@
+# Promenna s hodnotou 1
 counter = 1
-until counter > 11
-  puts counter
-  counter = counter + 1
 
+# Dokud neplati ( => je false), ze counter je vetsi nez 11
+until counter > 11
+
+  # Vytiskni hodnotu promenne
+  puts counter
+
+  # Zvys hodnotu promenne +1
+  counter = counter + 1
 end
