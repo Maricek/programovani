37091e45f51566dc978ff32c367a7c08794af41a
Mariana <trantinovamariana@gmail.com>
2013-08-16 21:26:09 +0200

    for_loop

---
+++ codeacademy/RUBY/loops/for_loop.rb
@@ -0,0 +1,5 @@
+for num in 1..15
+  # two dots .. include last integer, three dots ... say go up to but don't include 15
+  puts num
+  # For the variable num in the range 1 to 15, do the following." The following was to print "#{num}"
+end
